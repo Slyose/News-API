36e61741d749bdb0f78a3f63c3df619c2e765984
Slyose <modour498@gmail.com>
2022-08-02 14:26:14 +0100

    Removed duplicate tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -60,36 +60,6 @@ describe("GET /api/topics", () => {
   });
 });
 
-describe("GET /api/topics", () => {
-  test("should respond with an array", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then((response) => {
-        const data = response.body;
-        expect(Array.isArray(data.topics)).toBe(true);
-      });
-  });
-  test("should respond with an array of length 3", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then((response) => {
-        const data = response.body;
-        expect(data.topics.length).toBe(3);
-      });
-  });
-  test("should respond with an array of objects, with a key of slug and description", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then((response) => {
-        const data = response.body;
-        expect(data.topics).toEqual(idealOutputGetTopics);
-      });
-  });
-});
-
 describe("GET /api/articles/:article_id", () => {
   test("Should be able to return an object", () => {
     return request(app)
