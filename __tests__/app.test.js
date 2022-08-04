bde71e3581b64bd2d469d55a4f6c4de5b9ad8ce5
Slyose <modour498@gmail.com>
2022-08-04 11:24:53 +0100

    add GET /api/articles endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const request = require("supertest");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
+const sort = require("jest-sorted");
+const { string } = require("pg-format");
 
 afterAll(() => {
   return db.end();
@@ -217,6 +219,71 @@ describe("GET /api/users", () => {
       .expect(404)
       .then((response) => {
         expect(response.body.msg).toBe("Invalid endpoint.");
+      });
+  });
+});
+
+describe.only("GET /api/articles", () => {
+  test("should return an array", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(Array.isArray(response.body)).toBe(true);
+      });
+  });
+  test("should return an array with every article", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response._body.length).toBe(12);
+      });
+  });
+  test("should return an array sorted by created_at", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response._body).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
+  test("should return an array of objects with appropriate properties", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response._body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              article_id: expect.any(Number),
+              title: expect.any(String),
+              topic: expect.any(String),
+              author: expect.any(String),
+              created_at: expect.any(String),
+              comment_count: expect.any(Number),
+              votes: expect.any(Number),
+            }),
+          ])
+        );
+      });
+  });
+  test("should return an array of objects with appropriate properties", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response._body[0]).toEqual({
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          author: "icellusedkars",
+          created_at: "2020-11-03T09:12:00.000Z",
+          votes: 0,
+          comment_count: 2,
+        });
       });
   });
 });
